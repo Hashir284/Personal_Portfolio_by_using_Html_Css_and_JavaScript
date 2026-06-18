f9abafb077008ef73f3e1ca08bd177467ee949db
muhammadibsam63 <onlyforprofile1@gmail.com>
2026-06-18 02:32:46 -0700

    Responsive Completed

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -8,19 +8,75 @@ var typed = new Typed('.typing',
 )
 
 function locate(page, id) {
-    document.querySelectorAll('ul.nav a').forEach(e=>{
+
+    backSec()
+
+    document.querySelectorAll('ul.nav a').forEach(e => {
         e.classList.remove('active')
     })
     document.getElementById(id).classList.add('active')
     document.querySelectorAll('.sec').forEach(e => {
         if (page === e.id) {
-            console.log(e.id);
-            console.log(page);
-            console.log(id);
-            
             e.classList.remove('hidden')
         } else {
             e.classList.add('hidden')
         }
     })
+
+    if(window.innerWidth < 1307){
+        asideSectionToggler()
+    }
+
+
+    let allSec = document.querySelectorAll('.sec')
+    for (let i = 0; i < allSec.length; i++) {
+        let sec = allSec[i];
+        // remove active from all first
+        if (!sec.classList.contains(id.slice(0, -4))) {
+            sec.classList.remove('active');
+        } else {
+            sec.classList.add('active');
+        }
+    }
+}
+
+function backSec() {
+    // let backSec = document.querySelector('.sec.active')
+    //     backSec.classList.add('back-section')
+    //     document.querySelectorAll('.sec').forEach(e=>{
+    //     if(!e.classList.contains('active')){
+    //         e.classList.remove('back-section')
+    //         console.log(e);
+    //     }
+    // })
+
+    // or 
+
+    document.querySelectorAll('.sec').forEach(e => {
+        e.classList.remove('back-section');
+    });
+
+    let activeSec = document.querySelector('.sec.active');
+
+    if (activeSec) {
+        activeSec.classList.add('back-section');
+    }
+}
+
+const navToggleBtn = document.querySelector('.responsivenav'),
+    aside = document.querySelector('.aside')
+
+navToggleBtn.addEventListener('click', () => {
+    asideSectionToggler()
+})
+
+let flagVar = false
+const asideSectionToggler = () => {
+    aside.classList.toggle('open')
+    navToggleBtn.classList.toggle('open')
+let allSec = document.querySelectorAll('.sec')
+    for (let i = 0; i < allSec.length; i++) {
+        allSec[i].classList.toggle('open')
+    }
+
 }
